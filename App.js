c7eeaacdb14b5e612235491d21c4ca1ef0963cca
sooy0817 <93895846+sooy0817@users.noreply.github.com>
2023-04-13 16:28:54 +0900

    failed ..

--- App.js
+++ App.js
@@ -1,11 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import React from 'react';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+
+KEY ='959121096979ca4303e824e8451909f658ac8653';
+API_URL =`https://vision.googleapis.com/v1/images:annotate?key=$959121096979ca4303e824e8451909f658ac8653`;
+
+async function callGoogleVisionAsync(image) {
+  const body = {
+    requests: [
+      {
+        image: {
+          content: image,
+        },
+        features: [
+          {
+            type: 'LABEL_DETECTION',
+            maxResults: 1,
+          },
+        ],
+      },
+    ],
+  };
+
+  const response = await fetch('https://vision.googleapis.com/v1/images:annotate?key=$959121096979ca4303e824e8451909f658ac8653', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const result = await response.json();
+  console.log('callGoogleVisionAsync -> result', result);
+
+  return result.responses[0].labelAnnotations[0].description;
+}
 
 export default function App() {
+  const [image, setImage] = React.useState(null);
+  const [status, setStatus] = React.useState(null);
+
+
+
+  const takePictureAsync = async () => {
+    const { canceled, uri, base64 } = await ImagePicker.launchCameraAsync({
+      base64: true,
+    });
+
+    if (!canceled) {
+      setImage(uri);
+      setStatus('Loading...');
+      try {
+        const result = await callGoogleVisionAsync(base64);
+        setStatus(result);
+      } catch (error) {
+        setStatus(`Error: ${error.message}`);
+      }
+    } else {
+      setImage(null);
+      setStatus(null);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+        <>
+          {image && <Image style={styles.image} source={{ uri: image }} />}
+          {status && <Text style={styles.text}>{status}</Text>}
+          <Button onPress={takePictureAsync} title="Take a Picture" />
+        </>
+
     </View>
   );
 }
@@ -13,8 +78,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  text: {
+    margin: 5,
   },
 });
